4e99d387db99d938afb3fcb3ce45804d8d4cda9c
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2022-02-22 14:53:49 -0500

    Se suben todos los archivos relacionados al proyecto

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const publicPath = path.join(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  const home = path.join(__dirname, "./views/home.html");
+  res.sendFile(home);
+});
+app.get("/register", (req, res) => {
+  const register = path.join(__dirname, "./views/register.html");
+  res.sendFile(register);
+});
+app.get("/login", (req, res) => {
+  const login = path.join(__dirname, "./views/login.html");
+  res.sendFile(login);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`El servidor se está ejecutando en el puerto ${port}`)
+);
